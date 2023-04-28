0c11702c1491e2c88bf892bccf53c3e49d9429ea
Daniel <daniel.mazhbits@gmail.com>
2023-04-28 17:10:50 +0300

    syntax

--- server/config.js
+++ server/config.js
@@ -20,7 +20,6 @@ const AWS = require('aws-sdk');
 
 dotenv.config();
 
-const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
