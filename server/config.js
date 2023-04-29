d6821f60cab5117cdba38cbbaed9826057241e68
Daniel <daniel.mazhbits@gmail.com>
2023-04-29 23:45:37 +0300

    updated

--- server/config.js
+++ server/config.js
@@ -1,20 +1,3 @@
-// const dotenv = require('dotenv');
-// const AWS = require('aws-sdk');
-
-// dotenv.config();
-
-// module.exports = {
-//    JWT_SECRET: process.env.JWT_SECRET,
-//    siteUrl: "http://localhost:3000",
-// };
-
-// AWS.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   region: 'eu-central-1' // Replace with your desired AWS region, e.g., 'us-west-2'
-// });
-
-
 const dotenv = require('dotenv');
 const AWS = require('aws-sdk');
 
