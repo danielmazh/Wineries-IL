493eacef1c5365730342ce51e63ecc4c31bd0357
Daniel <daniel.mazhbits@gmail.com>
2023-04-29 23:09:33 +0300

    signatureVersion: 'v4'

--- server/config.js
+++ server/config.js
@@ -23,9 +23,9 @@ dotenv.config();
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
+  signatureVersion: 'v4' // Add this line
 });
-
 
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
@@ -35,6 +35,7 @@ module.exports = {
   AWS, // Export the AWS object
   AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME, // Add this line to export the S3 bucket name
 };
+
 
 
 
