79f5be47d41494eadca55197db8243a478ecadc2
Daniel <daniel.mazhbits@gmail.com>
2023-04-30 04:05:34 +0300

    logger update

--- server/services/UserData.js
+++ server/services/UserData.js
@@ -1,107 +1,17 @@
 // server\services\UserData.js
 
-// const format = require('pg-format');
-// const { query } = require("../db");
-
-// const { calculateScore } = require('./calculateScore');
-// const groupAndSortByArea = require('./groupByArea').groupAndSortByArea;
-
-// require('dotenv').config();
-
-
-// async function userData(storedData) {
-//   const queryParams = [];
-//   // let sqlQuery = `SELECT * FROM ${process.env.TABLE_NAME}.wineries WHERE main_area = %L AND average_cost_per_person < %L`;
-//   let sqlQuery = `SELECT * FROM ${process.env.TABLE_NAME}.wineries WHERE main_area = $1 AND average_cost_per_person < $2`;
-
-//   queryParams.push(storedData.TourArea, storedData.BudgetRange);
-
-//   const selectedDate = new Date(storedData.selectedDate);
-//   const day = selectedDate.toLocaleDateString('en-US', { weekday: 'long' }).toLowerCase();
-
-//   const timeRanges = [
-//     { name: 'TourTimeMorning', range: ['10:00', '12:00'] },
-//     { name: 'TourTimeAfternoon', range: ['12:00', '14:00'] },
-//     { name: 'TourTimeEvening', range: ['14:00', '16:00'] },
-//   ];
-  
-//   // Generate conditions for time ranges and the selected day
-//   const conditions = [];
-//   for (const timeRange of timeRanges) {
-//     if (storedData[timeRange.name]) {
-//       conditions.push(`(%L BETWEEN substring(oh_${day} from 1 for 5) AND substring(oh_${day} from 7 for 5))`);
-//       queryParams.push(timeRange.range[0]);
-//     }
-//   }
-
-//   if (conditions.length > 0) {
-//     sqlQuery += ` AND (${conditions.join(' OR ')})`;
-//   }
-
-//   console.log('queryParams:', queryParams);
-//   console.log('sqlQuery:', sqlQuery);
-
-//   return new Promise((resolve, reject) => {
-//     console.log('SQL Query:', sqlQuery); // Add this line
-//     console.log('Query Params:', queryParams); // Add this line
-//     query(sqlQuery, queryParams, (err, res) => {
-
-//     // query(format(sqlQuery, ...queryParams), (err, res) => {
-
-//       if (err) {
-//         console.error(err);
-//         reject(err);
-//       } else {
-//         const rows = res.rows;
-//         console.log('Raw Rows:', rows); // Add this line
-//         // Apply scoring and sorting
-//         const scoredRows = rows.map(row => {
-//           const scores = calculateScore(row, storedData);
-//           return { ...row, scores, weightedScore: scores.weightedScore };
-//         });
-
-//         const sortedRows = scoredRows.sort((a, b) => b.weightedScore - a.weightedScore);
-
-//         // Group the sorted rows by secondary area
-//         const groupedWineries = groupAndSortByArea(sortedRows);
-
-//         // Convert the groupedWineries object to an array of tables
-//         const tables = Object.entries(groupedWineries).map(([area, wineries]) => {
-//           return {
-//             area,
-//             wineries,
-//           };
-//         });
-
-//         // Return the tables array with grouped and sorted wineries
-//         resolve(tables);
-//       }
-//     });
-//   });
-// }
-
-// module.exports = {
-//   userData
-// };
-
-
-
-
-
-
-// server\services\UserData.js
-
 const format = require('pg-format');
 const { query } = require("../db");
 
 const { calculateScore } = require('./calculateScore');
 const groupAndSortByArea = require('./groupByArea').groupAndSortByArea;
 
-const logger = require('../../logger');
 require('dotenv').config();
+
 
 async function userData(storedData) {
   const queryParams = [];
+  // let sqlQuery = `SELECT * FROM ${process.env.TABLE_NAME}.wineries WHERE main_area = %L AND average_cost_per_person < %L`;
   let sqlQuery = `SELECT * FROM ${process.env.TABLE_NAME}.wineries WHERE main_area = $1 AND average_cost_per_person < $2`;
 
   queryParams.push(storedData.TourArea, storedData.BudgetRange);
@@ -114,7 +24,8 @@ async function userData(storedData) {
     { name: 'TourTimeAfternoon', range: ['12:00', '14:00'] },
     { name: 'TourTimeEvening', range: ['14:00', '16:00'] },
   ];
-
+  
+  // Generate conditions for time ranges and the selected day
   const conditions = [];
   for (const timeRange of timeRanges) {
     if (storedData[timeRange.name]) {
@@ -127,21 +38,23 @@ async function userData(storedData) {
     sqlQuery += ` AND (${conditions.join(' OR ')})`;
   }
 
-  logger.info('queryParams:', queryParams);
-  logger.info('sqlQuery:', sqlQuery);
+  console.log('queryParams:', queryParams);
+  console.log('sqlQuery:', sqlQuery);
 
   return new Promise((resolve, reject) => {
-    logger.info('SQL Query:', sqlQuery);
-    logger.info('Query Params:', queryParams);
+    console.log('SQL Query:', sqlQuery); // Add this line
+    console.log('Query Params:', queryParams); // Add this line
     query(sqlQuery, queryParams, (err, res) => {
 
+    // query(format(sqlQuery, ...queryParams), (err, res) => {
+
       if (err) {
-        logger.error('Error executing query:', err);
+        console.error(err);
         reject(err);
       } else {
         const rows = res.rows;
-        logger.info('Raw Rows:', rows);
-
+        console.log('Raw Rows:', rows); // Add this line
+        // Apply scoring and sorting
         const scoredRows = rows.map(row => {
           const scores = calculateScore(row, storedData);
           return { ...row, scores, weightedScore: scores.weightedScore };
@@ -149,8 +62,10 @@ async function userData(storedData) {
 
         const sortedRows = scoredRows.sort((a, b) => b.weightedScore - a.weightedScore);
 
+        // Group the sorted rows by secondary area
         const groupedWineries = groupAndSortByArea(sortedRows);
 
+        // Convert the groupedWineries object to an array of tables
         const tables = Object.entries(groupedWineries).map(([area, wineries]) => {
           return {
             area,
@@ -158,6 +73,7 @@ async function userData(storedData) {
           };
         });
 
+        // Return the tables array with grouped and sorted wineries
         resolve(tables);
       }
     });
@@ -167,3 +83,4 @@ async function userData(storedData) {
 module.exports = {
   userData
 };
+
--- server/views/UserData.js
+++ server/views/UserData.js
@@ -1,75 +1,33 @@
 // server\views\UserData.js
 
-// const UserDataService = require("../services/UserData");
-
-// async function userData(req, res) {
-//   const storedData = req.body;
-//   // console.log(storedData);
-
-//   try {
-//     UserDataService.storedFormData = storedData; // Store the form data in UserDataService
-//     // console.log(UserDataService.storedFormData);
-//     res.send(UserDataService.storedFormData);
-
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error confirming FormData");
-//   }
-// }
-
-
-// async function getUserData(req, res) {
-//   try {
-//     const storedData = UserDataService.storedFormData; // Get the stored form data
-//     const results = await UserDataService.userData(storedData); // Use the form data to fetch query results
-//     console.log('Results from DB:', results);
-
-//     res.send(JSON.stringify(results));
-
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).send("Error confirming FormData");
-//   }
-// }
-
-// module.exports = {
-//   userData , getUserData
-// };
-
-
-
-
-
-
-// server\views\UserData.js
-
 const UserDataService = require("../services/UserData");
-const logger = require('../../logger');
 
 async function userData(req, res) {
   const storedData = req.body;
+  // console.log(storedData);
 
   try {
     UserDataService.storedFormData = storedData; // Store the form data in UserDataService
+    // console.log(UserDataService.storedFormData);
     res.send(UserDataService.storedFormData);
 
   } catch (err) {
-    logger.error('Error confirming FormData:', err);
+    console.error(err);
     res.status(500).send("Error confirming FormData");
   }
 }
+
 
 async function getUserData(req, res) {
   try {
     const storedData = UserDataService.storedFormData; // Get the stored form data
     const results = await UserDataService.userData(storedData); // Use the form data to fetch query results
+    console.log('Results from DB:', results);
 
     res.send(JSON.stringify(results));
 
-    logger.info('Results from DB:', results);
-
   } catch (err) {
-    logger.error('Error confirming FormData:', err);
+    console.error(err);
     res.status(500).send("Error confirming FormData");
   }
 }
