2885f3f3fd77b5adf156e7cb56f16289a0db96f4
Daniel <daniel.mazhbits@gmail.com>
2023-05-01 09:58:28 +0300

    logger updates

--- logger.js
+++ logger.js
@@ -1,8 +1,32 @@
+// // logger.js
+// const winston = require('winston');
+
+// const logger = winston.createLogger({
+//   level: 'debug', // Set the log level here; for more details, use 'debug', for fewer, use 'info', 'warn', or 'error'
+//   format: winston.format.combine(
+//     winston.format.colorize(),
+//     winston.format.timestamp(),
+//     winston.format.printf(({ timestamp, level, message }) => {
+//       return `${timestamp} ${level}: ${message}`;
+//     })
+//   ),
+//   transports: [
+//     new winston.transports.Console(),
+//   ],
+// });
+
+// module.exports = logger;
+
+
+
+
+
 // logger.js
 const winston = require('winston');
+const path = require('path');
 
 const logger = winston.createLogger({
-  level: 'debug', // Set the log level here; for more details, use 'debug', for fewer, use 'info', 'warn', or 'error'
+  level: 'debug',
   format: winston.format.combine(
     winston.format.colorize(),
     winston.format.timestamp(),
@@ -13,6 +37,9 @@ const logger = winston.createLogger({
   transports: [
     new winston.transports.Console(),
   ],
+  fallbacks: {
+    path: require.resolve('path-browserify')
+  }
 });
 
 module.exports = logger;
