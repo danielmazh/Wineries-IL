d7f37673ba0eaea37edfa30574561c5d872a1383
Daniel <daniel.mazhbits@gmail.com>
2023-05-04 21:19:20 +0300

    page2

--- client/src/common/components/QuestionnairePages/Page2.js
+++ client/src/common/components/QuestionnairePages/Page2.js
@@ -166,7 +166,7 @@ const handleselectedWinesTypesOptions = (selectedOptions) => {
           <InputLabel style={{fontSize: "25px",}} htmlFor="KosherType">כשרות היקב </InputLabel>
         </Box>
 
-        <FormControl fullWidth style={{ width: "160px" }}>
+        <FormControl fullWidth style={{ width: "150px" }}>
 
           <label style={{ display: "flex", alignItems: "center", fontSize: "20px" }}>
 
